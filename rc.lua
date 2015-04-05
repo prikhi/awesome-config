98a57810afc2f3631d888d2dc803a8ffef0e40fe
Pavan Rikhi <pavan.rikhi+agit@gmail.com>
2015-04-05 18:22:11 -0400

    Cleanup Docs

--- rc.lua
+++ rc.lua
@@ -35,15 +35,7 @@ local r = require("runonce")
 local tyrannical = require("tyrannical")
 
 -- {{{ Error handling
--- Check if awesome encountered an error during startup and fell back to
--- another config (This code will only ever execute for the fallback config)
-if awesome.startup_errors then
-    naughty.notify({ preset = naughty.config.presets.critical,
-                     title = "Oops, there were errors during startup!",
-                     text = awesome.startup_errors })
-end
-
--- Handle runtime errors after startup
+-- Add a signal for runtime errors.
 do
     local in_error = false
     awesome.connect_signal("debug::error", function (err)
